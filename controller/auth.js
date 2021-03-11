11460ef80ba94f88809bf2ec4e2077ef5ba6967d
hasithaWalpola <walpolahasitha@gmail.com>
2021-03-11 23:07:58 +0530

    validation fixes

--- controller/auth.js
+++ controller/auth.js
@@ -1,55 +1,33 @@
 const bycrpt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const Users = require('../model/Users')
-// const User = require('../model/User')
 
 
 exports.login = async (req, res, next) => {
-    //Validation Feils
-    const { error } = loginValidation(req.body)
-    if (error) {
-        return res.status(400).send(error.details[0].message)
+
+    const checedkUser = await Users.findOne({ email: req.body.email })
+    if (!checedkUser) {
+        return res.status(400).send({ error: 'Account not exists' })
     }
 
-    //User Check
-    const userCheck = await Users.findOne({ email: req.body.email })
-    if (!userCheck) {
-        return res.status(400).send({ error: 'Seems like you dont have account' })
+    const isvalidPassword = await bycrpt.compare(req.body.password, checedkUser.password)
+    if (!isvalidPassword) {
+        return res.status(400).send({ error: 'Invdalid Password' })
     }
 
-    //PasswordComaprison
-    const validPassword = await bycrpt.compare(req.body.password, userCheck.password)
-    if (!validPassword) {
-        return res.status(400).send({ error: 'Incorrect Password' })
-    }
-
-
-    //creating a token
     const token = jwt.sign({ _id: userCheck._id }, process.env.TOKEN_SECRET)
-    res.header('auth-Token', token).send({ success: 'true', token: token, userId: userCheck._id, message: 'Member Login Sucessfull' })
+    res.header('auth-Token', token).send({ success: 'true', token: token, userId: userCheck._id, message: 'Login Successfull!' })
 }
 
 
 exports.registerUser = async (req, res) => {
-    //Validation
-    //console.log('here', req)
-   // const { error } = customerRegistrationValidation(req.body)
-    //console.log('error', error)
-    //console.log('here customer', customer)
-    // if (error) {
-    //     return res.status(400).json({ status: 400, message: error.details[0].message })
-    // }
 
-    //check user exist
-    const emailCheck = await Users.findOne({ email: req.body.email })
+    const checkedEmail = await Users.findOne({ email: req.body.email })
 
-    console.log('emailCheck', emailCheck)
-
-    if (emailCheck) {
-        return res.status(400).send({ status: 400, message: 'Email Already Exixts' })
+    if (checkedEmail) {
+        return res.status(400).send({ status: 400, message: 'Email Already Taken' })
     }
 
-    //Hash the password
     const salt = await bycrpt.genSalt(10);
     const hashedPassword = await bycrpt.hash(req.body.password, salt)
 
@@ -60,8 +38,6 @@ exports.registerUser = async (req, res) => {
         password: hashedPassword,
         mobile: req.body.mobile,
     });
-
-    console.log('here', user)
 
     try {
         const registerdUser = await user.save();
